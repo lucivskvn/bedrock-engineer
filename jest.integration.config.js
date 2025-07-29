f853a48bc586d663f2518546a2b55b16edc4fc04
Elang Swa Buana Putra <7908015+lucivskvn@users.noreply.github.com>
2025-07-29 11:31:39 +0800

    chore(jest): transform ESM dependencies

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,18 +1,29 @@
 require('dotenv').config()
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+const esModules = [
+  'node-fetch',
+  'data-uri-to-buffer',
+  'fetch-blob',
+  'formdata-polyfill',
+  'node-domexception',
+  'web-streams-polyfill'
+]
+
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: 90000,
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules.join('|')})`],
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
-        tsconfig: 'tsconfig.test.json'
+        tsconfig: 'tsconfig.test.json',
+        useESM: false
       }
-    ]
+    ],
+    '^.+\\.[jt]sx?$': 'babel-jest'
   },
   testMatch: ['**/*.integration.test.ts'],
   setupFiles: ['<rootDir>/jest.integration.setup.js']
