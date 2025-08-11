70594192f5897db719fd87face4a9ee2376f1fe7
Elang Swa Buana Putra <7908015+lucivskvn@users.noreply.github.com>
2025-08-11 13:52:48 +0800

    chore: upgrade eslint v9 and migrate config

--- eslint.config.js
+++ eslint.config.js
@@ -1,55 +1,51 @@
 import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  resolvePluginsRelativeTo: __dirname,
-  recommendedConfig: js.configs.recommended
-})
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import prettier from 'eslint-config-prettier/flat'
+import globals from 'globals'
 
 export default [
   {
     ignores: ['node_modules', 'dist', 'out', '.gitignore']
   },
-  ...compat
-    .config({
-      extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        '@electron-toolkit/eslint-config-ts/recommended',
-        '@electron-toolkit/eslint-config-prettier'
-      ],
-      settings: {
-        react: {
-          version: 'detect'
-        }
-      },
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
-        ],
-        'react/prop-types': 'off',
-        'no-control-regex': 0,
-        'no-restricted-syntax': [
-          'warn',
-          {
-            selector: 'ImportExpression',
-            message:
-              'Consider using static imports instead of dynamic imports unless specifically required.'
-          }
-        ]
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
       }
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}']
-    }))
+    }
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  prettier,
+  {
+    files: ['**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}'],
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+      ],
+      'react/prop-types': 'off',
+      'no-control-regex': 0,
+      'no-restricted-syntax': [
+        'warn',
+        {
+          selector: 'ImportExpression',
+          message:
+            'Consider using static imports instead of dynamic imports unless specifically required.'
+        }
+      ]
+    }
+  }
 ]
+
